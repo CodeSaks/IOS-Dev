c3b4e10fc00ac99528e6d5d7ad215079759b9195
Saksham <sakshammahajan243@gmail.com>
2024-02-12 12:39:41 -0800

    Adding Full Egg Timer

--- EggTimer-iOS13/EggTimer/ViewController.swift
+++ EggTimer-iOS13/EggTimer/ViewController.swift
@@ -7,9 +7,52 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
     
-
-
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+        
+    @IBOutlet weak var progressBar: UIProgressView!
+    
+    let eggTimes = ["Soft" : 30, "Medium" : 50, "Hard" : 70 ]
+    
+    var player: AVAudioPlayer?
+    var counter : Int = 60
+    var totalTime = 0
+    var secondsPassed = 0
+    var timer = Timer()
+    
+    @objc func updateCounter() {
+        //example functionality
+        if secondsPassed < totalTime {
+            secondsPassed += 1
+            progressBar.progress = Float(secondsPassed)/Float(totalTime)
+        }else{
+            timer.invalidate()
+            titleLabel.text = "DONE!"
+            let url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3")
+            player = try! AVAudioPlayer(contentsOf: url!)
+        player?.play()
+        }
+    }
+    
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        
+        timer.invalidate()
+        
+               
+        let hardness = sender.currentTitle!
+        
+        titleLabel.text =  hardness
+        
+        totalTime = eggTimes[hardness]!
+        secondsPassed = 0
+        progressBar.progress = 0.0
+        
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
+    }
+    
 }
